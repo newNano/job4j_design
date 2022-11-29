e7c6b9ab7629c185bd0f3d224cc958377614b4d9
11_pochta@bk.ru <11_pochta@bk.ru>
2022-11-29 21:34:43 +0600

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,88 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        modCount++;
+        if (size == container.length) {
+            container = Arrays.copyOf(container, container.length * 2 + 1);
+        }
+        container[size] = value;
+        size++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        int i = Objects.checkIndex(index, container.length);
+        T oldValue = container[i];
+        container[i] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        modCount++;
+        int i = Objects.checkIndex(index, container.length);
+        T removedValue = container[i];
+        System.arraycopy(container,
+                        i + 1,
+                        container,
+                        i,
+                        container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        return removedValue;
+    }
+
+    @Override
+    public T get(int index) {
+        int i = Objects.checkIndex(index, size);
+        return container[i];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private final int expectedModCount = modCount;
+            private int index;
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                if (!this.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[index++];
+            }
+        };
+    }
+}
