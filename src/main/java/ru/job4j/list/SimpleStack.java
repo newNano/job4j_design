30518f946e944e2b87644b6a72232c06e1a36e48
11_pochta@bk.ru <11_pochta@bk.ru>
2022-12-02 12:54:44 +0600

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/list/SimpleStack.java
+++ src/main/java/ru/job4j/list/SimpleStack.java
@@ -2,12 +2,19 @@ package ru.job4j.list;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private int size;
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
+        size++;
         linked.addFirst(value);
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
