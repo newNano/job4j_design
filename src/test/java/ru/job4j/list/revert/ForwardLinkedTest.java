cdfcdfdd7291536fac189cd93b54ecd1f9361a7b
11_pochta@bk.ru <11_pochta@bk.ru>
2022-12-02 15:46:11 +0600

    6. Перевернуть связанный список [#161]

---
+++ src/test/java/ru/job4j/list/revert/ForwardLinkedTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.list.revert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.NoSuchElementException;
+
+class ForwardLinkedTest {
+    private ForwardLinked<Integer> linked;
+
+    @BeforeEach
+    void init() {
+        linked = new ForwardLinked<>();
+    }
+
+    @Test
+    void whenSize0ThenReturnFalse() {
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenSize1ThenReturnFalse() {
+        linked.add(1);
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenAddAndRevertTrue() {
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
+        assertThat(linked).containsSequence(1, 2, 3, 4);
+        assertThat(linked.revert()).isTrue();
+        assertThat(linked).containsSequence(4, 3, 2, 1);
+    }
+}
