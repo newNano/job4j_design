9901e250b111a4fd2d618af73eaf6ed53887fb3e
nano <11_pochta@bk.ru>
2026-03-07 17:11:59 +0600

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 1; i < 10; i++) {
+                output.write((1 + " * " + i + " = " + (1 * i)).getBytes());
+                output.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
